3d944fe36936d20c5738dfb4a5bffe27db902341
Arnau Siches <asiches@gmail.com>
2015-08-15 22:44:35 +0100

    Add checkout() and pull()

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,42 @@ extern crate git2;
 extern crate docopt;
 extern crate rustc_serialize;
 
-use std::str;
+use std::process::{Command, exit};
+
 use docopt::Docopt;
-use git2::{Repository, Error, StatusOptions, ErrorCode, SubmoduleIgnore};
+use git2::{Repository, Error, StatusOptions, ErrorCode};
+
+
+
 
 #[derive(RustcDecodable)]
 struct Args {
     arg_spec: Vec<String>,
     flag_git_dir: Option<String>,
 }
+
+/// Gets the current branch.
+///
+/// # Examples
+///
+/// ```
+/// let repo = try!(Repository::open("."));
+/// let branchname = try!(branch(&repo));
+/// ```
+fn branch<'a>(repo: &'a Repository) -> Result<String, Error> {
+    let head = match repo.head() {
+        Ok(head) => Some(head),
+            Err(ref e) if e.code() == ErrorCode::UnbornBranch ||
+                          e.code() == ErrorCode::NotFound
+                       => None,
+            Err(e) => return Err(e),
+    };
+    let branchref = head.as_ref().and_then(|h| h.shorthand());
+    let branchname = branchref.unwrap_or("HEAD (no branch)").to_string();
+
+    Ok(branchname)
+}
+
 
 fn run(args: &Args) -> Result<(), Error> {
     let path = args.flag_git_dir.clone().unwrap_or(".".to_string());
@@ -28,112 +55,62 @@ fn run(args: &Args) -> Result<(), Error> {
         opts.pathspec(spec);
     }
 
-    loop {
-        let statuses = try!(repo.statuses(Some(&mut opts)));
-        let current_branch = try!(branch(&repo));
+    let current_branch = try!(branch(&repo));
 
-        println!("Current branch {:?}", current_branch);
+    println!("Current branch {:?}", current_branch);
 
-        print_short(&repo, statuses);
+    checkout("master");
+    pull("origin", "master");
+    checkout(current_branch);
+    // rebase("master");
 
-        return Ok(())
+    return Ok(())
+}
+
+fn checkout(target_branch: &str) {
+    let output = Command::new("git")
+                         .arg("checkout")
+                         .arg(target_branch)
+                         .output()
+                         .unwrap_or_else(|e| {
+                            panic!("failed to execute process: {}", e) });
+    let status_code = output.status.code();
+
+    println!("Checking out {} ...", target_branch);
+
+    match status_code {
+        Some(0) => println!("{}", String::from_utf8_lossy(&output.stdout)),
+        Some(_) => {
+             println!("{}", String::from_utf8_lossy(&output.stderr));
+             exit(status_code.unwrap());
+        }
+        None => println!("Something wrong happened"),
     }
 }
 
-fn branch<'a>(repo: &'a Repository) -> Result<String, Error> {
-    let head = match repo.head() {
-        Ok(head) => Some(head),
-            Err(ref e) if e.code() == ErrorCode::UnbornBranch ||
-                          e.code() == ErrorCode::NotFound
-                       => None,
-            Err(e) => return Err(e),
-    };
-    let branchref = head.as_ref().and_then(|h| h.shorthand());
-    let branchname = branchref.unwrap_or("HEAD (no branch)").to_string();
+fn pull(target_remote: &str, target_branch: &str) {
+    let output = Command::new("git")
+                         .arg("pull")
+                         .arg("--quiet")
+                         .arg("--rebase=preserve")
+                         .arg(target_remote)
+                         .arg(target_branch)
+                         .output()
+                         .unwrap_or_else(|e| {
+                            panic!("failed to execute process: {}", e) });
+    let status_code = output.status.code();
 
-    Ok(branchname)
-}
+    println!("Pulling {} {} ...", target_remote, target_branch);
 
-// This version of the output prefixes each path with two status columns and
-// shows submodule status information.
-fn print_short(repo: &Repository, statuses: git2::Statuses) {
-    for entry in statuses.iter().filter(|e| e.status() != git2::STATUS_CURRENT) {
-        let mut istatus = match entry.status() {
-            s if s.contains(git2::STATUS_INDEX_NEW) => 'A',
-            s if s.contains(git2::STATUS_INDEX_MODIFIED) => 'M',
-            s if s.contains(git2::STATUS_INDEX_DELETED) => 'D',
-            s if s.contains(git2::STATUS_INDEX_RENAMED) => 'R',
-            s if s.contains(git2::STATUS_INDEX_TYPECHANGE) => 'T',
-            _ => ' ',
-        };
-        let mut wstatus = match entry.status() {
-            s if s.contains(git2::STATUS_WT_NEW) => {
-                if istatus == ' ' { istatus = '?'; } '?'
-            }
-            s if s.contains(git2::STATUS_WT_MODIFIED) => 'M',
-            s if s.contains(git2::STATUS_WT_DELETED) => 'D',
-            s if s.contains(git2::STATUS_WT_RENAMED) => 'R',
-            s if s.contains(git2::STATUS_WT_TYPECHANGE) => 'T',
-            _ => ' ',
-        };
-
-        if entry.status().contains(git2::STATUS_IGNORED) {
-            istatus = '!';
-            wstatus = '!';
+    match status_code {
+        Some(0) => println!("{}", String::from_utf8_lossy(&output.stdout)),
+        Some(_) => {
+             println!("{}", String::from_utf8_lossy(&output.stderr));
+             exit(status_code.unwrap());
         }
-        if istatus == '?' && wstatus == '?' { continue }
-        let mut extra = "";
-
-        // TODO: check for GIT_FILEMODE_COMMIT
-        let status = entry.index_to_workdir().and_then(|diff| {
-            let ignore = SubmoduleIgnore::Unspecified;
-            diff.new_file().path_bytes()
-                .and_then(|s| str::from_utf8(s).ok())
-                .and_then(|name| repo.submodule_status(name, ignore).ok())
-        });
-        if let Some(status) = status {
-            if status.contains(git2::SUBMODULE_STATUS_WD_MODIFIED) {
-                extra = " (new commits)";
-            } else if status.contains(git2::SUBMODULE_STATUS_WD_INDEX_MODIFIED) {
-                extra = " (modified content)";
-            } else if status.contains(git2::SUBMODULE_STATUS_WD_WD_MODIFIED) {
-                extra = " (modified content)";
-            } else if status.contains(git2::SUBMODULE_STATUS_WD_UNTRACKED) {
-                extra = " (untracked content)";
-            }
-        }
-
-        let (mut a, mut b, mut c) = (None, None, None);
-        if let Some(diff) = entry.head_to_index() {
-            a = diff.old_file().path();
-            b = diff.new_file().path();
-        }
-        if let Some(diff) = entry.index_to_workdir() {
-            a = a.or(diff.old_file().path());
-            b = b.or(diff.old_file().path());
-            c = diff.new_file().path();
-        }
-
-        match (istatus, wstatus) {
-            ('R', 'R') => println!("RR {} {} {}{}", a.unwrap().display(),
-                                   b.unwrap().display(), c.unwrap().display(),
-                                   extra),
-            ('R', w) => println!("R{} {} {}{}", w, a.unwrap().display(),
-                                 b.unwrap().display(), extra),
-            (i, 'R') => println!("{}R {} {}{}", i, a.unwrap().display(),
-                                 c.unwrap().display(), extra),
-            (i, w) => println!("{}{} {}{}", i, w, a.unwrap().display(), extra),
-        }
-    }
-
-    for entry in statuses.iter().filter(|e| e.status() == git2::STATUS_WT_NEW) {
-        println!("?? {}", entry.index_to_workdir().unwrap().old_file()
-                               .path().unwrap().display());
+        None => println!("Something wrong happened"),
     }
 }
-
-// impl Args {
-// }
 
 fn main() {
     const USAGE: &'static str = "
